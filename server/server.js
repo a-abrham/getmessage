da186de0a838c2b1cd64006804c4edaa9cf3d2c2
abrshkorra <abrshkorra@gmail.com>
2024-01-03 09:57:31 +0300

    added route, enabled cors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { setupWebhook } = require('./telegram-bot');
+const messageRoute = require('./messageRoute');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const port = 3005;
 
 app.use(bodyParser.json());
+app.use('/', messageRoute);
+
 
 setupWebhook(app);
 
